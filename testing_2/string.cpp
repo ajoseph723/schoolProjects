b6e9a29e4c47c60ad5c6d714a19b09ff0bd9ad21
ajosep24 <ajosep24@f32d0f4a-0a5c-491f-9104-be04ea66fa5e>
2023-10-12 21:15:41 +0000

    turning in lab 7

--- testing_2/string.cpp
+++ testing_2/string.cpp
@@ -1,65 +1,77 @@
-//File:        string.cpp   
-//       
-//Version:     1.0
-//Date:        10/9/2023 - Current Version Kent State University
+// File:        string.cpp
 //
-//Author:      Dr. J. Maletic
+// Version:     1.0
+// Date:        10/9/2023 - Current Version Kent State University
 //
-//Description: -Implementation of simple String class member functions. 
-//             
-
+// Author:      Dr. J. Maletic
+//
+// Description: -Implementation of simple String class member functions.
+//
 
 #include "string.hpp"
 
-
 //////////////////////////////////////////////////////////
 // Default Constructor
-// ENSURES: str[0] == 0 
+// ENSURES: str[0] == 0
 //
-String::String() { 
-      str[0] = 0;
-} 
-
-
-////////////////////////////////////////////////////////// 
-// ENSURES: str == s 
-// Example:  String("abcd")
-//
-String::String(const char *s) { 
-    int i = 0;
-    while (s[i] != 0) {
-        str[i] = s[i];
-        i++;
-        if (i >= DEFAULT_STRING_CAPACITY-1) break;
-    }
-    str[i] = 0;
-} 
-
+String::String()
+{
+    str[0] = 0;
+}
 
 //////////////////////////////////////////////////////////
-//Less Than
+// ENSURES: str == s
+// Example:  String("abcd")
 //
-bool String::operator<(const String& rhs)  const {
-    int  i = 0;
-    
-    //Find first non equal characters
-    while ((str[i] != 0) && (rhs.str[i] != 0) && (str[i] == rhs.str[i]))  ++i;
-    
-    if (str[i] == 0) return true;                           //"aa" < "aaaaa"
-    if (str[i] < rhs.str[i]) return true;                   //"aa" < "ab"
+String::String(const char *s)
+{
+    int i = 0;
+    while (s[i] != 0)
+    {
+        str[i] = s[i];
+        i++;
+        if (i >= DEFAULT_STRING_CAPACITY - 1)
+            break;
+    }
+    str[i] = 0;
+}
+
+String::String(const char ch)
+{
+    str[0] = ch;
+    str[1] = 0;
+}
+
+//////////////////////////////////////////////////////////
+// Less Than
+//
+bool String::operator<(const String &rhs) const
+{
+    int i = 0;
+
+    // Find first non equal characters
+    while ((str[i] != 0) && (rhs.str[i] != 0) && (str[i] == rhs.str[i]))
+        ++i;
+
+    if (str[i] == 0 && rhs.str[i] != 0)
+        return true; //"aa" < "aaaaa"
+    if (str[i] < rhs.str[i])
+        return true; //"aa" < "ab"
     return false;
 }
 
-
-bool operator<(const char *lhs, const String& rhs) {
+bool operator<(const char *lhs, const String &rhs)
+{
     return String(lhs) < rhs;
 }
 
-bool operator<(const char lhs, const String& rhs) {
+bool operator<(const char lhs, const String &rhs)
+{
     return String(lhs) < rhs;
 }
 
-std::ostream& operator<<(std::ostream& out, String rhs) {
+std::ostream &operator<<(std::ostream &out, String rhs)
+{
     out << rhs.str;
     return out;
 }
