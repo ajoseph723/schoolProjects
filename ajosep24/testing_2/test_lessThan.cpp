5b46e8b3b5c0c149c0c3e973adab976089876098
ajosep24 <ajosep24@f32d0f4a-0a5c-491f-9104-be04ea66fa5e>
2023-10-12 21:15:41 +0000

    turning in lab 7

--- ajosep24/testing_2/test_lessThan.cpp
+++ ajosep24/testing_2/test_lessThan.cpp
@@ -1,16 +1,66 @@
 #include <iostream>
 #include "string.hpp"
 
-int main (){
+int main()
+{
 
 	{
+		// testing 2 default strings
 
+		String a;
+		String b;
+
+		assert(!(a < b) && !(b < a));
 	}
 
 	{
+		// testing 2 equal strings that contain a char
 
+		String a('a');
+		String b('b');
+		String c('a');
+
+		assert(a < b);
+		assert(!(b < a));
+		assert(!(a < c) && !(c < a));
 	}
 
+	{
+		// testing string of character arrays of the same size
+		String a("abc");
+		String b("abc");
+		String c("abb");
 
-return 0;
+		assert(!(a < b));
+		assert(c < a);
+	}
+
+	{
+		// testing strings of different sizes
+		String a("abc");
+		String b("abcd");
+		String c("aabb");
+
+		assert(a < b);
+		assert(c < a);
+	}
+
+	{
+		// testing strings less than different data types and vice versa
+		String a("abc");
+		String b("abcd");
+		String c('a');
+
+		// Verify
+		// testing String < char[]
+		assert(a < "abcd");
+		assert(!("abcd" < a));
+		// testing String < String
+		assert(a < b);
+		// testing String < char
+		assert(c < 'b');
+		assert(!('b' < c));
+	}
+
+	return 0;
 }
