cd420bc1a3f7f861732b46ca664c4a6f5f02d792
ajosep24 <ajosep24@f32d0f4a-0a5c-491f-9104-be04ea66fa5e>
2023-10-22 04:03:54 +0000

    LogEntry constructor complete

--- ajosep24/string/logentry.cpp
+++ ajosep24/string/logentry.cpp
@@ -1,57 +1,142 @@
 //
 //
-// File:        logentry.cpp  
-//       
+// File:        logentry.cpp
+//
 // Version:     1.0
-// Date:        
-// Author:      
+// Date:
+// Author:
 //
 // Description: Class implementation for a log entry.
 //
 //
- 
+
 #include "string.hpp"
 #include "logentry.hpp"
 #include <iostream>
 #include <vector>
 
-// REQUIRES:
-// ENSURES: 
-//
-LogEntry::LogEntry(String s) {
-    // ...
-    std::vector<String> vec = s.split(' ');
-    
+void Date::setYear(String s)
+{
+    int temp = 0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        temp = temp + (s[i] - '0');
+        temp = temp * 10;
+    }
+    temp = temp / 10;
+    year = temp;
+}
+
+Time::Time(String hr, String min, String sec)
+{
+    int temp = 0;
+    for (int i = 0; i < hr.length(); ++i)
+    {
+        temp = temp + (hr[i] - '0');
+        temp = temp * 10;
+    }
+    temp = temp / 10;
+    hour = temp;
+    temp = 0;
+
+    for (int i = 0; i < min.length(); ++i)
+    {
+        temp = temp + (min[i] - '0');
+        temp = temp * 10;
+    }
+    temp = temp / 10;
+    minute = temp;
+    temp = 0;
+
+    for (int i = 0; i < sec.length(); ++i)
+    {
+        temp = temp + (sec[i] - '0');
+        temp = temp * 10;
+    }
+    temp = temp / 10;
+    second = temp;
 }
 
 // REQUIRES:
-// ENSURES: 
+// ENSURES:
 //
-std::vector<LogEntry> parse(std::istream& in) {
+LogEntry::LogEntry(String s)
+{
+    // making the string into a vector of strings
+    std::vector<String> vec = s.split(' ');
+
+    // checking to see if the input is a valid logentry
+    if (vec.size() != 10)
+    {
+        *this = LogEntry();
+    }
+    else
+    {
+        host = vec[0];
+
+        // setting the Date
+        Date tempDate;
+        std::vector<String> dateVec = vec[3].split('/');
+        std::vector<String> dayVec = dateVec[0].split('[');
+
+        date.setDay(dayVec[1]);
+        date.setMonth(dateVec[2]);
+
+        std::vector<String> yearVec = dateVec[2].split(':');
+
+        date.setYear(yearVec[0]);
+
+        // setting the time
+        Time tempTime(yearVec[1], yearVec[2], yearVec[3]);
+        time = tempTime;
+
+        // getting request
+        std::vector<String> requestVec = s.split('"');
+        request = requestVec[1];
+
+        // setting status
+        status = vec[8];
+
+        // setting number_of_bytes
+        String bytes = vec[9];
+        for (int i = 0; i < bytes.length(); ++i)
+        {
+            number_of_bytes = number_of_bytes + (bytes[i] - '0');
+            number_of_bytes = number_of_bytes * 10;
+        }
+        number_of_bytes = number_of_bytes / 10;
+    }
+}
+
+// REQUIRES:
+// ENSURES:
+//
+std::vector<LogEntry> parse(std::istream &in)
+{
     std::vector<LogEntry> result;
 
-    return result;
+        return result;
 }
 
 // REQUIRES:
-// ENSURES: 
+// ENSURES:
 //
-void output_all(std::ostream& out, const std::vector<LogEntry> &) {
-    
+void output_all(std::ostream &out, const std::vector<LogEntry> &)
+{
 }
 
 // REQUIRES:
-// ENSURES: 
+// ENSURES:
 //
-void by_host(std::ostream& out, const std::vector<LogEntry>&) {
-
+void by_host(std::ostream &out, const std::vector<LogEntry> &)
+{
 }
 
 /// REQUIRES:
-// ENSURES: 
+// ENSURES:
 //
-int byte_count(const std::vector<LogEntry> &) {
-    
+int byte_count(const std::vector<LogEntry> &)
+{
+
     return 0;
 }
-
--- ajosep24/string/logentry.hpp
+++ ajosep24/string/logentry.hpp
@@ -9,7 +9,7 @@
 //
 //
 //
- 
+
 #ifndef CS_LOGENTRY_H_
 #define CS_LOGENTRY_H_
 
@@ -17,48 +17,51 @@
 #include <iostream>
 #include <vector>
 
-class Date {
+class Date
+{
 public:
-            Date() {};
-private:
-    String  day, month;
-    int     year;
-};
-
-
-class Time {
-  public:
-            Time() {};
-  private:
-    int     hour, minute, second;
-};
-
-
-
-class LogEntry {
-public:
-            LogEntry() {};
-            LogEntry(String);
-    friend  std::ostream& operator<<(std::ostream&, const LogEntry&);
+    Date(){};
+    void setDay(String s) { day = s; };
+    void setMonth(String s) { month = s; };
+    void setYear(String);
 
 private:
-    String  host;
-    Date    date;
-    Time    time;
-    String  request;
-    String  status;
-    int     number_of_bytes;
+    String day, month;
+    int year;
 };
 
+class Time
+{
+public:
+    Time(){};
+
+private:
+    int hour, minute, second;
+};
+
+class LogEntry
+{
+public:
+    LogEntry(){};
+    LogEntry(String);
+    friend std::ostream &operator<<(std::ostream &, const LogEntry &);
+
+private:
+    String host;
+    Date date;
+    Time time;
+    String request;
+    String status;
+    int number_of_bytes;
+};
 
 //
 // Free functions
 //
 
-std::vector<LogEntry>   parse       (std::istream&);
-void                    output_all  (std::ostream&, const std::vector<LogEntry> &);
-void                    by_host     (std::ostream&, const std::vector<LogEntry> &);
-int                     byte_count  (const std::vector<LogEntry>&);
+std::vector<LogEntry> parse(std::istream &);
+void output_all(std::ostream &, const std::vector<LogEntry> &);
+void by_host(std::ostream &, const std::vector<LogEntry> &);
+int byte_count(const std::vector<LogEntry> &);
 
 #endif
-
