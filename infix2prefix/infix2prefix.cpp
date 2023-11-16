3264c96c4ec26188511ff33a926e4773d8aaacc4
ajosep24 <ajosep24@f32d0f4a-0a5c-491f-9104-be04ea66fa5e>
2023-11-16 20:12:10 +0000

    submitting lab 11

--- infix2prefix/infix2prefix.cpp
+++ infix2prefix/infix2prefix.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "stack.hpp"
-#include "string.hpp"
+#include "../assembler/stack.hpp"
+#include "../string/string.hpp"
 #include <fstream>
 
 String inToPre(String &original);
