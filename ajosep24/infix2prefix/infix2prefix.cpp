1b918a9330769b4bdeea41e1c2ac6b63743c7ba8
ajosep24 <ajosep24@f32d0f4a-0a5c-491f-9104-be04ea66fa5e>
2023-11-16 20:06:38 +0000

    lab 11 files

---
+++ ajosep24/infix2prefix/infix2prefix.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "stack.hpp"
+#include "string.hpp"
+#include <fstream>
+
+String inToPre(String &original);
+
+int main() {
+    std::ifstream inputFile("data3-1.txt");
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Error: Can not open file" << std::endl;
+        exit(1);
+    }
+
+    String infix;
+    char ch;
+    inputFile.get(ch);
+    while (!inputFile.eof())
+    {
+        if (ch != '\n')
+        {
+            infix += ch;
+        }
+        else
+        {
+            String postfix = inToPre(infix);
+            std::cout << postfix << std::endl;
+            infix = "";
+        }
+        inputFile.get(ch);
+    }
+
+    inputFile.close();
+
+    return 0;
+}
+
+
+
+
+String inToPre(String &original)
+{
+    stack<String> input;
+    int i = 0;
+    String temp;
+    String right;
+    String oper;
+    String left;
+
+    while (original[i] != ';')
+    {
+        if (original[i] == ')')
+        {
+            right = input.pop();
+            oper = input.pop();
+            left = input.pop();
+            input.push(oper +left + right);
+        }
+        else if (original[i] != '(' && original[i] != ' ')
+            temp += original[i];
+        else if (original[i] == ' ' && temp.length() > 0)
+        {
+            temp += ' ';
+            input.push(temp);
+            temp = "";
+        }
+        ++i;
+    }
+    return input.pop();
+}
